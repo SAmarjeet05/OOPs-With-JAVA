a918a66521bf2b201d51d92062a6fad200b6fbef
SAmarjeet05 <amar2005.reet@gmail.com>
2025-04-19 18:50:40 +0530

    Renamed everything

--- Java Codes/src/inheritance/SingleLevel.java
+++ Java Codes/src/inheritance/SingleLevel.java
@@ -1,10 +1,10 @@
 package inheritance;
 
 
-class parent{
+class Parent{
 	int i;
 	
-	parent(int i){
+	Parent(int i){
 		System.out.println("The value of i is: "+i);
 	}
 	
@@ -15,29 +15,29 @@ class parent{
 
 
 
-class child extends parent{
+class Child extends Parent{
 	
 	String name;
 	int i;
 	
-	child(String name,int i){
+	Child(String name,int i){
 		super(i); //accessing parent class first
 		System.out.println(name+""+" is the child of parent "+i);
 	}
 
-	void cdisplay() {
+	void cDisplay() {
 		System.out.println("This is child class");
 	}
 	
 }
 
-public class singleLevel{
+public class SingleLevel{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		child s1 = new child("Ram",3); 
+		Child s1 = new Child("Ram",3); 
 		s1.display();
-		s1.cdisplay();
+		s1.cDisplay();
 
 	}
 
