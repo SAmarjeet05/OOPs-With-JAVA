a1a14af3b5534e37201338227ca2fc16cf13c050
SAmarjeet05 <amar2005.reet@gmail.com>
2025-04-24 18:06:25 +0530

    Added new files
    Added CmdLineArgs.java
    Added JaggedArr.java

---
+++ Java Codes/src/com/amarjeet/CmdLineArgs.java
@@ -0,0 +1,30 @@
+package com.amarjeet;
+
+public class CmdLineArgs {
+
+	public static void main(String[] args) {
+		
+		if(args.length == 0) {
+			System.out.println("No number entered!!");
+		}
+		
+		int number = Integer.parseInt(args[0]);
+		
+		boolean prime = true;
+		
+		for(int i=2;i<number;i++) {
+			if(number%i==0) {
+				prime = false;
+				break;
+			}
+		}
+		
+		if(prime) {
+			System.out.printf("The number %d is a prime number.",number);
+		}else {
+			System.out.printf("The number %d is not a prime number.",number);
+		}
+		
+	}
+
+}
---
+++ Java Codes/src/com/amarjeet/JaggedArr.java
@@ -0,0 +1,35 @@
+package com.amarjeet;
+import java.util.Scanner;
+
+public class JaggedArr {
+
+	public static void main(String[] args) {
+		int i,j,k;
+		
+		System.out.println("How many rows do you want: ");
+		Scanner sc = new Scanner(System.in);
+		int row = sc.nextInt();
+		int jaggedarr[][] = new int[row][];
+		
+		for(i=0;i<row;i++) {
+			System.out.printf("Enter the size of column for %d row: ",i);
+			jaggedarr[i] = new int[sc.nextInt()];
+		}
+		System.out.println("Enter the elements of the jagged array: ");
+		for(j=0;j<jaggedarr.length;j++) {
+			for(k=0;k<jaggedarr[j].length;k++) {
+				System.out.printf("Enter the value of jaggedarr[%d][%d]: ",j,k);
+				jaggedarr[j][k] = sc.nextInt();
+			}
+		}
+		System.out.println("Printing the elements of the array: ");
+		for(j=0;j<jaggedarr.length;j++) {
+			for(k=0;k<jaggedarr[j].length;k++) {
+				System.out.printf("%d ",jaggedarr[j][k]);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
