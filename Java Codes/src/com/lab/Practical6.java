37d69e545468b8e555789822fbf2bcf2a01a7ed1
SAmarjeet05 <amar2005.reet@gmail.com>
2025-05-05 16:55:09 +0530

    Update Practical6.java

--- Java Codes/src/com/lab/Practical6.java
+++ Java Codes/src/com/lab/Practical6.java
@@ -34,12 +34,12 @@ public class Practical6 {
 	public static void main(String[] args) {
 		
 		ArithmaticOp a = new ArithmaticOp();
-		System.out.println(a.add(34,67.7));
-		System.out.println(a.sub(89,56.34));
-		System.out.println(a.mul(34,67.7));
+		System.out.println("Adding 34 and 67.7: "+a.add(34,67.7));
+		System.out.println("Subtracting 56.34 from 89: "+a.sub(89,56.34));
+		System.out.println("Multiplying 34 with 67.7: "+a.mul(34,67.7));
 		
 		MethodOverride m = new MethodOverride();
-		System.out.println(m.add(34,67.7));
+		System.out.println("Adding 34 and 67.7 to 10: "+m.add(34,67.7));
 	}
 
 }
